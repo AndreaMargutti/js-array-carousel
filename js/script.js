86cc476b7e4058edea15d30dff01598891891304
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-20 15:42:14 +0200

    first active image

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('ok');
+// ! Creiamo le immagini in modo dinamico
+// creo le immagini e recupero la galleria
+const carouselGallery = document.getElementById('gallery');
+const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 
+'img/05.webp']
+
+//creo stringa vuota
+let img = '';
+
+for ( let i = 0; i < sources.length; i++) {
+    img += `<img src="${sources[i]}">`
+}
+
+carouselGallery.innerHTML = img;
+console.log(carouselGallery);
+
+//recupero le immagini
+const images = document.querySelectorAll('#carousel img');
+console.log(images);
+
+//Rendiamo active la prima immagine
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add('active');
