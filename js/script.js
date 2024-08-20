793bab9e49ee1d86bcba126caf9d2374a18368ac
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-20 16:48:01 +0200

    esercizio finito con bonus

--- js/script.js
+++ js/script.js
@@ -12,17 +12,20 @@ for ( let i = 0; i < sources.length; i++) {
     img += `<img src="${sources[i]}">`
 }
 
+//inserisco le immagini
 carouselGallery.innerHTML = img;
 thumbnails.innerHTML = img;
-console.log(carouselGallery);
+
 
 //recupero le immagini
 const images = document.querySelectorAll('#carousel img');
+const thumbimg = document.querySelectorAll('#thumbnails img');
 console.log(images);
 
 //Rendiamo active la prima immagine
 let currentActiveIndex = 0;
 images[currentActiveIndex].classList.add('active');
+thumbimg[currentActiveIndex].classList.add('active');
 
 // # Eventi Dinamici
 //recupero i due bottoni
@@ -33,6 +36,7 @@ const nextBtn = document.getElementById('next');
 nextBtn.addEventListener('click', function(){
     //rimuovo active da immagine attiva
     images[currentActiveIndex].classList.remove('active');
+    thumbimg[currentActiveIndex].classList.remove('active');
     
     //incremento di uno l'index
     currentActiveIndex++;
@@ -42,12 +46,14 @@ nextBtn.addEventListener('click', function(){
 
     //aggiungo classe actiuve su prossima immagine
     images[currentActiveIndex].classList.add('active');
+    thumbimg[currentActiveIndex].classList.add('active');
 })
 
 //creazione evento per pre
 preBtn.addEventListener('click', function() {
     //rimuovo active da img attiva
     images[currentActiveIndex].classList.remove('active')
+    thumbimg[currentActiveIndex].classList.remove('active');
 
     //decremento currentActiveIndex di 1
     currentActiveIndex--;
@@ -57,4 +63,21 @@ preBtn.addEventListener('click', function() {
 
     //inserisco active in immagine precednete
     images[currentActiveIndex].classList.add('active')
+    thumbimg[currentActiveIndex].classList.add('active');
 })
+
+// ! Faccio in modo che al click sulla thumb cambi l'immagine nel carosello
+
+for(let i = 0; i < thumbimg.length; i++) {
+    const thumb = thumbimg[i];
+
+    thumb.addEventListener('click', function() {
+        images[currentActiveIndex].classList.remove('active')
+        thumbimg[currentActiveIndex].classList.remove('active');
+
+        currentActiveIndex = i;
+    
+        images[currentActiveIndex].classList.add('active')
+        thumbimg[currentActiveIndex].classList.add('active');
+    })
+}
